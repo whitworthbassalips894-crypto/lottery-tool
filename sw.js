f49072894d6614ff25488aa2098e729300640a00
whitworthbassalips894-crypto <whitworthbassalips894@gmail.com>
2026-07-13 11:24:55 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+
+const CACHE_NAME = 'tmsys-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png',
+  './favicon-32.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) { return response; }
+      return fetch(event.request);
+    })
+  );
+});
